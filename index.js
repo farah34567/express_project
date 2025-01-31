0ba6afdcaf579d4c0b1abecdf9dbd0f6c494f236
farah34567 <mohamedalifarah177@gmail.com>
2025-01-31 19:03:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts",(req, res)=>{
+    res.json({message:"get all contacts "});
+})
+
+app.listen(port, ()=>{
+    console.log('server running on port ${PORT}');
+});
